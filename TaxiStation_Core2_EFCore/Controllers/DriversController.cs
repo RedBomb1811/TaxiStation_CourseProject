fbe2be0af169e8df2b151510d33127394bbad0cb
redbomb1811 <dimadudko00@gmail.com>
2019-05-07 09:47:48 +0300

    start | working application

---
+++ TaxiStation_Core2_EFCore/Controllers/DriversController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TestExample.DB;
+
+namespace TaxiStation_Core2_EFCore.Controllers
+{
+    public class DriversController : Controller
+    {
+        private readonly TaxiStationContext _context;
+
+        public DriversController(TaxiStationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Drivers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Drivers.ToListAsync());
+        }
+
+        // GET: Drivers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var drivers = await _context.Drivers
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (drivers == null)
+            {
+                return NotFound();
+            }
+
+            return View(drivers);
+        }
+
+        // GET: Drivers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Drivers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,first_name,second_name,birth_date,sex,phone_number,pass_number,rating_sum,rating_count,email,pass_hash")] Drivers drivers)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(drivers);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(drivers);
+        }
+
+        // GET: Drivers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var drivers = await _context.Drivers.FindAsync(id);
+            if (drivers == null)
+            {
+                return NotFound();
+            }
+            return View(drivers);
+        }
+
+        // POST: Drivers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,first_name,second_name,birth_date,sex,phone_number,pass_number,rating_sum,rating_count,email,pass_hash")] Drivers drivers)
+        {
+            if (id != drivers.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(drivers);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DriversExists(drivers.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(drivers);
+        }
+
+        // GET: Drivers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var drivers = await _context.Drivers
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (drivers == null)
+            {
+                return NotFound();
+            }
+
+            return View(drivers);
+        }
+
+        // POST: Drivers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var drivers = await _context.Drivers.FindAsync(id);
+            _context.Drivers.Remove(drivers);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DriversExists(int id)
+        {
+            return _context.Drivers.Any(e => e.id == id);
+        }
+    }
+}
