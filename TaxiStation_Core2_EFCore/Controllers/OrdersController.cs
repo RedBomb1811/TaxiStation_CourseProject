e5ba53cacb25351abd81250b2a0246d96b9bdfb8
redbomb1811 <dimadudko00@gmail.com>
2019-05-08 01:12:24 +0300

    Before MakeOrder

--- TaxiStation_Core2_EFCore/Controllers/OrdersController.cs
+++ TaxiStation_Core2_EFCore/Controllers/OrdersController.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using TaxiStation_Core2_EFCore.Models.ViewModels;
 using TestExample.DB;
 
 namespace TaxiStation_Core2_EFCore.Controllers
 {
+    [Route("Order")]
     public class OrdersController : Controller
     {
         private readonly TaxiStationContext _context;
@@ -18,135 +20,21 @@ namespace TaxiStation_Core2_EFCore.Controllers
             _context = context;
         }
 
-        // GET: Orders
-        public async Task<IActionResult> Index()
+        [HttpGet]
+        [Route("MakeOrder")]
+        public IActionResult MakeOrder()
         {
-            return View(await _context.Orders.ToListAsync());
+            ViewBag.Order_types = new SelectList(_context.Order_types, "id", "name");
+            return View("MakeOrder");
         }
 
-        // GET: Orders/Details/5
-        public async Task<IActionResult> Details(long? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var orders = await _context.Orders
-                .FirstOrDefaultAsync(m => m.id == id);
-            if (orders == null)
-            {
-                return NotFound();
-            }
-
-            return View(orders);
-        }
-
-        // GET: Orders/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Orders/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("id,id_client,id_turns,date_start_order,date_start_trip,date_end_trip,id_order_type,start_point_lat,start_point_long,end_point_lat,end_point_long,security_code,confirmed,pets,child")] Orders orders)
+        [Route("MakeOrder")]
+        public IActionResult MakeOrder(MakeOrder order)
         {
-            if (ModelState.IsValid)
-            {
-                _context.Add(orders);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(orders);
-        }
-
-        // GET: Orders/Edit/5
-        public async Task<IActionResult> Edit(long? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var orders = await _context.Orders.FindAsync(id);
-            if (orders == null)
-            {
-                return NotFound();
-            }
-            return View(orders);
-        }
-
-        // POST: Orders/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(long id, [Bind("id,id_client,id_turns,date_start_order,date_start_trip,date_end_trip,id_order_type,start_point_lat,start_point_long,end_point_lat,end_point_long,security_code,confirmed,pets,child")] Orders orders)
-        {
-            if (id != orders.id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(orders);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!OrdersExists(orders.id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(orders);
-        }
-
-        // GET: Orders/Delete/5
-        public async Task<IActionResult> Delete(long? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var orders = await _context.Orders
-                .FirstOrDefaultAsync(m => m.id == id);
-            if (orders == null)
-            {
-                return NotFound();
-            }
-
-            return View(orders);
-        }
-
-        // POST: Orders/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(long id)
-        {
-            var orders = await _context.Orders.FindAsync(id);
-            _context.Orders.Remove(orders);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool OrdersExists(long id)
-        {
-            return _context.Orders.Any(e => e.id == id);
+            ///TODO: MakeOrder
+            ViewBag.Order_types = new SelectList(_context.Order_types, "id", "name");
+            return View("MakeOrder");
         }
     }
 }
--- TaxiStation_Core2_EFCore/Models/ViewModels/MakeOrder.cs
+++ TaxiStation_Core2_EFCore/Models/ViewModels/MakeOrder.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,13 +8,31 @@ namespace TaxiStation_Core2_EFCore.Models.ViewModels
 {
     public class MakeOrder 
     {
+        [Key]
+        [Required]
+        [Display(Name = "Номер телефона")]
+        [DataType(DataType.PhoneNumber)]
         public string Phone_number { get; set; }
+        [Required]
+        [Display(Name = "Тип заказа")]
         public int Id_order_type { get; set; }
+        [Required]
+        [Display(Name = "Начало поездки 1")]
         public float Start_point_lat { get; set; }
+        [Required]
+        [Display(Name = "Начало поездки 2")]
         public float Start_point_long { get; set; }
+        [Required]
+        [Display(Name = "Конец поездки 1")]
         public float End_point_lat { get; set; }
+        [Required]
+        [Display(Name = "Конец поездки 2")]
         public float End_point_long { get; set; }
+        [Required]
+        [Display(Name = "С животными")]
         public bool Pets { get; set; }
+        [Required]
+        [Display(Name = "с детьми")]
         public bool Child { get; set; }
 
     }
